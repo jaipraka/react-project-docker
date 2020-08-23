f7596e57ecb2bdd3d9385bd9363f9d728f8a4aa1
prakash <prakash.akuthota@rediffmail.com>
2020-08-23 03:16:08 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
 
-          Edit <code>src/App.js</code> and save to reload by A Prakash.
+          Edit <code>src/App.js</code> and save to reload by A Prakash From Feature.
      
         </p>
         <a
